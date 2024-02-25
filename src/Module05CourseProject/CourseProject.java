3934f76bfb4e8e79b9be664b476f9d59b7196268
Austtiin <austinstephens103@gmail.com>
2024-02-25 16:16:41 -0600

    commit

---
+++ src/Module05CourseProject/CourseProject.java
@@ -0,0 +1,142 @@
+
+package Module05CourseProject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+interface Patient{
+	public int CalScore();
+	public int CalBMI();
+}
+
+
+ class Person implements Patient{
+	String Name, Birthday, Weight, Height;
+	String InsuranceCat = null;
+	public int CalScore(int InBMI) {
+		
+		if(InBMI <= 18.5)
+		{
+			this.InsuranceCat = "Low";
+			return 0;
+			
+		}
+		if(InBMI > 18.6 & InBMI < 24.9)
+		{
+			this.InsuranceCat = "Low";
+			return 1;
+		}
+		if(InBMI > 25 & InBMI < 29.9)
+		{
+			this.InsuranceCat = "High";
+			return 2;
+		}
+		if(InBMI > 29.9)
+		{
+			this.InsuranceCat = "Highest";
+			return 3;
+		}
+		return InBMI;
+		
+	}
+	
+	
+	public int CalBMI() {
+		Float OUT = Float.parseFloat(this.Weight) / Float.parseFloat(this.Height) / Float.parseFloat(this.Height) * 703;
+		return OUT.intValue();
+		
+	}
+
+
+	@Override
+	public int CalScore() {
+		return 0;
+	}
+	
+	
+	 
+	
+}
+
+ class PersonBMIScore extends Person  {
+	String[] BMIScore = {"Underweight", "Normal-Weight", "Over-Weight", "Obesity"};
+	int OURBMINUM;
+	void WriteTO() throws IOException {
+		File fout = new File("C:/JavaFiles/OutputM04.txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+	 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		
+			bw.write(this.Name);
+			bw.newLine();
+			bw.write(this.Weight);
+			bw.newLine();
+			bw.write(this.Height);
+			bw.newLine();
+			bw.write(this.Birthday);
+			bw.newLine();
+			bw.write(this.InsuranceCat);
+			bw.newLine();
+			bw.write(this.BMIScore[OURBMINUM]);
+			bw.newLine();
+			bw.newLine();
+			bw.close();
+	}
+}
+
+public abstract class CourseProject extends Person{
+
+	public static void main(String[] args) {
+		
+		Scanner UserInput = new Scanner(System.in);
+		PersonBMIScore Person1 = new PersonBMIScore();
+		
+		
+		try {
+			
+		while(true) {
+			
+			while(!UserInput.equals("q")) {
+				System.out.println("Please Enter Name, q to exit");
+				Person1.Name = UserInput.nextLine();
+				if (Person1.Name.equals("q")) {
+					break;
+				}
+			
+		System.out.println("Please Enter Height in Inches");
+		Person1.Height = UserInput.nextLine();
+		
+		System.out.println("Please Enter Wight In LBS");
+		Person1.Weight = UserInput.nextLine();
+		
+		System.out.println("Please Enter Birthday");
+		Person1.Birthday = UserInput.nextLine();
+		
+		System.out.println("\n");
+		System.out.println("\n Welcome " + Person1.Name);
+		
+		System.out.println("Based On You Input You Are In The Range Of: ");
+		System.out.println(Person1.BMIScore[Person1.CalScore(Person1.CalBMI())]);
+		Person1.OURBMINUM = Person1.CalScore(Person1.CalBMI());
+		System.out.println("And Have an Insurance Payment of: ");
+		System.out.println(Person1.InsuranceCat);
+		Person1.WriteTO();
+		
+		}
+			break;
+		}
+		
+		
+		
+		
+		}catch(Exception e) {}
+		
+		}
+		
+		
+	}
+
+
---
+++ src/Module05CourseProjectProject/Test.java
@@ -0,0 +1,9 @@
+package Module05CourseProjectProject;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
